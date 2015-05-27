4747a6226ec1b0a404ca716872331e15e6979381
Loafter <mopper@yandex.ru>
2015-05-27 17:00:42 +0300

    run service

--- f_comp_test.go
+++ f_comp_test.go
@@ -9,7 +9,7 @@ func TestFc(t *testing.T) {
 		t.Errorf("error: wrong param test failed ")
 	}
 
-	if err := fc.CompressFolder("7z.exe", "a -tzip", "\"C:\\Temp\\\"", "\"C:\\Temp\\temp.zip\""); err != nil {
+	if err := fc.CompressFolder("7z.exe", "ta -tzip", "\"C:\\Temp\\\"", "\"C:\\Temp\\temp.zip\""); err != nil {
 		t.Errorf("error: normal compress failed ")
 		log.Println(err)
 	}
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "log"
 
 func main() {
+	EaSrvCmp := EaFolderCompressorSrc{}
+	log.Println("gui located add http://localhost:9980/index.html")
+	EaSrvCmp.Start(9980)
 
-	log.Println("gui located add http://localhost:9979/index.html")
 }
